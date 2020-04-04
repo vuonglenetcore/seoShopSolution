40e9df586dbff203eaf990b083d93f152f68c215
Admin <Admin@DESKTOP-K9E5QJ9>
2020-04-04 13:50:19 +0700

    bài 3 tao solution - dbcontext + data - migrations

---
+++ seoShopSolution.Data/EF/seoShopSolutionDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace seoShopSolution.Data.EF
+{
+    public class seoShopSolutionDbContextFactory : IDesignTimeDbContextFactory<seoShopSolutionDbContext>
+    {
+        public seoShopSolutionDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("seoShopSolutionDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<seoShopSolutionDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new seoShopSolutionDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ seoShopSolution.Data/Enums/Status.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace seoShopSolution.Data.Enums
+{
+    public enum Status
+    {
+        InActive,
+        Active
+    }
+}
